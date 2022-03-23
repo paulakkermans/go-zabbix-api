9e682f642862480de47842634be5ec16831cc403
p.akkermans <p.akkermans@lukkien.com>
2022-03-23 15:54:49 +0100

    service.go in the making

---
+++ service.go
@@ -0,0 +1,170 @@
+package zabbix
+
+type (
+	// AvailableType (readonly) Availability of Zabbix agent
+	// see "available" in: https://www.zabbix.com/documentation/3.2/manual/api/reference/host/object
+	AvailableType int
+
+	// StatusType Status and function of the host.
+	// see "status" in:	https://www.zabbix.com/documentation/3.2/manual/api/reference/host/object
+	StatusType int
+)
+
+const (
+	// Unknown (default)
+	Unknown AvailableType = 0
+	// Available host is available
+	Available AvailableType = 1
+	// Unavailable host is unavailable
+	Unavailable AvailableType = 2
+)
+
+const (
+	// Monitored monitored host(default)
+	Monitored StatusType = 0
+	// Unmonitored unmonitored host
+	Unmonitored StatusType = 1
+)
+
+// Host represent Zabbix host object
+// https://www.zabbix.com/documentation/3.2/manual/api/reference/host/object
+type Host struct {
+	HostID    string        `json:"hostid,omitempty"`
+	Host      string        `json:"host"`
+	Available AvailableType `json:"available,string"`
+	Error     string        `json:"error"`
+	Name      string        `json:"name"`
+	Status    StatusType    `json:"status,string"`
+
+	// Fields below used only when creating hosts
+	GroupIds    HostGroupIDs   `json:"groups,omitempty"`
+	Interfaces  HostInterfaces `json:"interfaces,omitempty"`
+	TemplateIDs TemplateIDs    `json:"templates,omitempty"`
+}
+
+// Hosts is an array of Host
+type Hosts []Host
+
+// HostsGet Wrapper for host.get
+// https://www.zabbix.com/documentation/3.2/manual/api/reference/host/get
+func (api *API) HostsGet(params Params) (res Hosts, err error) {
+	if _, present := params["output"]; !present {
+		params["output"] = "extend"
+	}
+	err = api.CallWithErrorParse("host.get", params, &res)
+	return
+}
+
+// HostsGetByHostGroupIds Gets hosts by host group Ids.
+func (api *API) HostsGetByHostGroupIds(ids []string) (res Hosts, err error) {
+	return api.HostsGet(Params{"groupids": ids})
+}
+
+// HostsGetByHostGroups Gets hosts by host groups.
+func (api *API) HostsGetByHostGroups(hostGroups HostGroups) (res Hosts, err error) {
+	ids := make([]string, len(hostGroups))
+	for i, id := range hostGroups {
+		ids[i] = id.GroupID
+	}
+	return api.HostsGetByHostGroupIds(ids)
+}
+
+// HostGetByID Gets host by Id only if there is exactly 1 matching host.
+func (api *API) HostGetByID(id string) (res *Host, err error) {
+	hosts, err := api.HostsGet(Params{"hostids": id})
+	if err != nil {
+		return
+	}
+
+	if len(hosts) == 1 {
+		res = &hosts[0]
+	} else {
+		e := ExpectedOneResult(len(hosts))
+		err = &e
+	}
+	return
+}
+
+// HostGetByHost Gets host by Host only if there is exactly 1 matching host.
+func (api *API) HostGetByHost(host string) (res *Host, err error) {
+	hosts, err := api.HostsGet(Params{"filter": map[string]string{"host": host}})
+	if err != nil {
+		return
+	}
+
+	if len(hosts) == 1 {
+		res = &hosts[0]
+	} else {
+		e := ExpectedOneResult(len(hosts))
+		err = &e
+	}
+	return
+}
+
+// HostsCreate Wrapper for host.create
+// https://www.zabbix.com/documentation/3.2/manual/api/reference/host/create
+func (api *API) HostsCreate(hosts Hosts) (err error) {
+	response, err := api.CallWithError("host.create", hosts)
+	if err != nil {
+		return
+	}
+
+	result := response.Result.(map[string]interface{})
+	hostids := result["hostids"].([]interface{})
+	for i, id := range hostids {
+		hosts[i].HostID = id.(string)
+	}
+	return
+}
+
+// HostsUpdate Wrapper for host.update
+// https://www.zabbix.com/documentation/3.2/manual/api/reference/host/update
+func (api *API) HostsUpdate(hosts Hosts) (err error) {
+	_, err = api.CallWithError("host.update", hosts)
+	return
+}
+
+// HostsDelete Wrapper for host.delete
+// Cleans HostId in all hosts elements if call succeed.
+// https://www.zabbix.com/documentation/3.2/manual/api/reference/host/delete
+func (api *API) HostsDelete(hosts Hosts) (err error) {
+	ids := make([]string, len(hosts))
+	for i, host := range hosts {
+		ids[i] = host.HostID
+	}
+
+	err = api.HostsDeleteByIds(ids)
+	if err == nil {
+		for i := range hosts {
+			hosts[i].HostID = ""
+		}
+	}
+	return
+}
+
+// HostsDeleteByIds Wrapper for host.delete
+// https://www.zabbix.com/documentation/3.2/manual/api/reference/host/delete
+func (api *API) HostsDeleteByIds(ids []string) (err error) {
+	hostIds := make([]map[string]string, len(ids))
+	for i, id := range ids {
+		hostIds[i] = map[string]string{"hostid": id}
+	}
+
+	response, err := api.CallWithError("host.delete", hostIds)
+	if err != nil {
+		// Zabbix 2.4 uses new syntax only
+		if e, ok := err.(*Error); ok && e.Code == -32500 {
+			response, err = api.CallWithError("host.delete", ids)
+		}
+	}
+	if err != nil {
+		return
+	}
+
+	result := response.Result.(map[string]interface{})
+	hostids := result["hostids"].([]interface{})
+	if len(ids) != len(hostids) {
+		err = &ExpectedMore{len(ids), len(hostids)}
+	}
+	return
+}
